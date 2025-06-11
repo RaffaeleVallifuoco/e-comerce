7ff8a5db0e9a2ab9a6ebb3ca650b6243ea3311bc
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2025-06-11 13:31:10 +0200

    creating models

---
+++ e-commerce/src/test/java/it/raffo/e_commerce/model/Prodotto.java
@@ -0,0 +1,206 @@
+package it.raffo.e_commerce.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Prodotto")
+public class Prodotto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "descrizione", length = 600, nullable = true)
+    private String descrizione;
+
+    @Column(name = "prezzo", nullable = false)
+    private Double prezzo;
+
+    @Column(name = "quantita", nullable = false)
+    private Integer quantita;
+
+    @Column(name = "data_produzione", nullable = false)
+    private LocalDate dataProduzione;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "marca_id", nullable = false)
+    private Marca marca;
+
+    // CONSTRUCTORS
+
+    public Prodotto() {
+    }
+
+    public Prodotto(Integer id, String nome, String descrizione, Double prezzo, Integer quantita,
+            LocalDate dataProduzione, Categoria categoria, Marca marca) {
+        this.id = id;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.quantita = quantita;
+        this.dataProduzione = dataProduzione;
+        this.categoria = categoria;
+        this.marca = marca;
+    }
+
+    // GETTER & SETTER
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Integer getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(Integer quantita) {
+        this.quantita = quantita;
+    }
+
+    public LocalDate getDataProduzione() {
+        return dataProduzione;
+    }
+
+    public void setDataProduzione(LocalDate dataProduzione) {
+        this.dataProduzione = dataProduzione;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    // TOSTRING - EQUALS - HASHCODE
+
+    @Override
+    public String toString() {
+        return "Prodotto [id=" + id + ", nome=" + nome + ", descrizione=" + descrizione + ", prezzo=" + prezzo
+                + ", quantita=" + quantita + ", dataProduzione=" + dataProduzione + ", categoria=" + categoria
+                + ", marca=" + marca + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((descrizione == null) ? 0 : descrizione.hashCode());
+        result = prime * result + ((prezzo == null) ? 0 : prezzo.hashCode());
+        result = prime * result + ((quantita == null) ? 0 : quantita.hashCode());
+        result = prime * result + ((dataProduzione == null) ? 0 : dataProduzione.hashCode());
+        result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+        result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Prodotto other = (Prodotto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (descrizione == null) {
+            if (other.descrizione != null)
+                return false;
+        } else if (!descrizione.equals(other.descrizione))
+            return false;
+        if (prezzo == null) {
+            if (other.prezzo != null)
+                return false;
+        } else if (!prezzo.equals(other.prezzo))
+            return false;
+        if (quantita == null) {
+            if (other.quantita != null)
+                return false;
+        } else if (!quantita.equals(other.quantita))
+            return false;
+        if (dataProduzione == null) {
+            if (other.dataProduzione != null)
+                return false;
+        } else if (!dataProduzione.equals(other.dataProduzione))
+            return false;
+        if (categoria == null) {
+            if (other.categoria != null)
+                return false;
+        } else if (!categoria.equals(other.categoria))
+            return false;
+        if (marca == null) {
+            if (other.marca != null)
+                return false;
+        } else if (!marca.equals(other.marca))
+            return false;
+        return true;
+    }
+
+}

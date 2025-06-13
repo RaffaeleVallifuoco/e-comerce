8000c305da316278900cea17ca897ab06e4a220d
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2025-06-13 12:17:39 +0200

    FIX: navbar search method

--- e-commerce/src/main/java/it/raffo/e_commerce/repository/ProdottoRepo.java
+++ e-commerce/src/main/java/it/raffo/e_commerce/repository/ProdottoRepo.java
@@ -10,7 +10,7 @@ import it.raffo.e_commerce.model.Prodotto;
 
 public interface ProdottoRepo extends JpaRepository<Prodotto, Integer> {
 
-    @Query(value = "SELECT * FROM prodotto WHERE LOWER(nome) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(descrizione) LIKE LOWER(CONCAT('%', :keyword, '%') )", nativeQuery = true)
+    @Query("SELECT p FROM Prodotto p WHERE LOWER(p.nome) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.descrizione) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(p.marca.nome) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Prodotto> cercaProdotti(@Param("keyword") String keyword);
 
     List<Prodotto> findByCategoriaId(Integer categoriaId);

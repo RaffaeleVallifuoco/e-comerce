474878b1b6580dbb0d04f2bcb13095e1fa114bcf
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2025-06-14 18:20:46 +0200

    EDIT: detail page

--- e-commerce/src/main/java/it/raffo/e_commerce/controller/ProductController.java
+++ e-commerce/src/main/java/it/raffo/e_commerce/controller/ProductController.java
@@ -1,15 +1,24 @@
 package it.raffo.e_commerce.controller;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.raffo.e_commerce.model.Prodotto;
+import it.raffo.e_commerce.repository.CategoryRepo;
 import it.raffo.e_commerce.repository.ProdottoRepo;
+import it.raffo.e_commerce.repository.MarcaRepo;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/product")
@@ -18,15 +27,85 @@ public class ProductController {
     @Autowired
     ProdottoRepo productRepo;
 
+    @Autowired
+    CategoryRepo categoryRepo;
+
+    @Autowired
+    MarcaRepo marcaRepo;
+
+    // FIND BY ID
     @GetMapping("/show/{id}")
-    public String getProductById(@PathVariable("category_id") Integer id,
+    public String getProductById(@PathVariable("id") Integer id,
             Model model) {
 
         Prodotto product = productRepo.getReferenceById(id);
 
         model.addAttribute("product", product);
+        model.addAttribute("evidence", productRepo.findByEvidenceTrue());
 
-        return "/common/detail";
+        return "/home/detail";
+
+    }
+
+    // UPDATE
+    @GetMapping("/{id}/edit")
+    public String getEditForm(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("ticket", productRepo.getReferenceById(id));
+        model.addAttribute("evidence", productRepo.findByEvidenceTrue());
+        return "/tab/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String Update(@PathVariable("id") Integer id, @Valid @ModelAttribute("ticket") Prodotto productUpdate,
+            BindingResult bindingresult,
+            Model model) {
+
+        if (bindingresult.hasErrors()) {
+            model.addAttribute("evidence", productRepo.findByEvidenceTrue());
+
+            return "/tab/edit";
+        }
+        Prodotto existingProduct = productRepo.getReferenceById(id);
+
+        existingProduct.setNome(productUpdate.getNome());
+        existingProduct.setDescrizione(productUpdate.getDescrizione());
+        existingProduct.setEvidence(productUpdate.isEvidence());
+        existingProduct.setCategoria(productUpdate.getCategoria());
+        existingProduct.setMarca(productUpdate.getMarca());
+        existingProduct.setPrezzo(productUpdate.getPrezzo());
+        existingProduct.setQuantita(productUpdate.getQuantita());
+        existingProduct.setDataProduzione(productUpdate.getDataProduzione());
+
+        productRepo.save(existingProduct);
+
+        return "redirect:/show/{id}";
+    }
+
+    // DELETE
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Integer id) {
+
+        productRepo.deleteById(id);
+
+        return "redirect:/tab/index";
+    }
+
+    // CREATE
+    @PostMapping("/insert")
+    public String store(@Valid @ModelAttribute("product") Prodotto productForm, BindingResult bindingresult,
+            Model model) {
+
+        if (bindingresult.hasErrors()) {
+
+            model.addAttribute("category", categoryRepo.findAll());
+            model.addAttribute("brand", marcaRepo.findAll());
+
+            return "/admin/create";
+        }
+
+        productRepo.save(productForm);
+
+        return "redirect:/tab/index";
 
     }
 
